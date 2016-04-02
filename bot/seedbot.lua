453fa15a80ea1f1c607f73054f074ece6f86dcc9
kinehbot <rinlogger12349@yahoo.com>
2016-04-03 01:32:14 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -233,8 +233,11 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {92307266,103649648,111020322,0,tonumber(our_id)},--Sudo users
-    moderation = {data = 'data/moderation.json'} 
+    sudo_users = {92307266,0,tonumber(our_id)},--Sudo users
+    moderation = {data = 'data/moderation.json'},
+    about_text = [[Teleseed v4
+
+]],
     help_text_realm = [[
 Realm Commands:
 !creategroup [Name]
